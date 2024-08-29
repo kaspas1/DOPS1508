1eb51968b747374086458e180ca6bd5bef348801
Disraely Omar Pastor Solórzano <titodops@gmail.com>
2024-08-29 07:53:29 -0600

    Finalización de código HTML, Finalización de hoja de estilos, Inicio de lógica del negocio

---
+++ js/converter.js
@@ -0,0 +1,11 @@
+const inputField = document.getElementById("input-temp") //la constante devuelve propiedades del elemento con id input-temp, con class se pueden repetir el elemento
+console.log(inputField); //se puede colocar cualquier mensaje entre (), la constante creada hereda las propiedades del elemento
+const fromUnitField = document.getElementById("input-unit");
+const toUnitField = document.getElementById("output-unit");
+const outputField = document.getElementById("output-temp");
+const form = document.getElementById("converter");
+
+console.log("Disraely Omar Pastor Solórzano");
+
+
+
